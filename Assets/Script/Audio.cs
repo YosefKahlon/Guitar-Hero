97a88811513faa38114621dc7aa7a84406933537
yosefkahlon <yosefkahlon53@gmail.com>
2023-04-14 14:50:11 +0300

    audio button

--- Assets/Script/Audio.cs
+++ Assets/Script/Audio.cs
@@ -6,8 +6,10 @@ using UnityEngine.InputSystem;
 public class Audio : MonoBehaviour
 {
     [SerializeField] AudioSource audio;
-    [SerializeField] bool play;
-
+    [SerializeField] bool play = true;
+    [SerializeField] InputAction playButton = new InputAction(type: InputActionType.Button);
+    [SerializeField] InputAction stopButton = new InputAction(type: InputActionType.Button);
+    private float audioTime = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +17,44 @@ public class Audio : MonoBehaviour
         
     }
 
-    
+    void OnEnable()
+    {
+        playButton.Enable();
+        stopButton.Enable();
+    }
+
+    void OnDisable()
+    {
+        playButton.Disable();
+        stopButton.Disable();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (play)
         {
-            audio.Play();
+            if (playButton.WasPressedThisFrame())
+            {
+                if (audioTime > 0f) // Resume from where the audio left off
+                {
+                    audio.time = audioTime;
+                }
+                else
+                {
+                    audio.Play(); // Start from the beginning if it's the first time playing
+                }
+                play = false;
+            }
+        }
+        else
+        {
+            if (stopButton.WasPressedThisFrame())
+            {
+                audioTime = audio.time; // Store the current playback time
+                play = true;
+                audio.Stop();
+            }
         }
     }
 }
